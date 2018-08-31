afaee125eea88232ee82ce9f96533ec84757ed5e
leenan <leena@infiniteit.biz>
2018-08-31 16:59:23 +0530

    cart images

--- mall/resources/views/Frontend/pages/cart/index.blade.php
+++ mall/resources/views/Frontend/pages/cart/index.blade.php
@@ -22,9 +22,6 @@
                             <th class="text-left">Product</th>
                             <th>Price <span class="currency-sym-in-braces"></span></th>
                             <th>Quantity</th>
-                            @if(@$feature['coupon'] == 1)
-                            <th>Coupon Discount <span class="currency-sym-in-braces"></span></th>
-                            @endif
                             @if(@$feature['tax'] == 1)
                             <th>Tax <span class="currency-sym-in-braces"></span></th>
                             @endif
@@ -102,11 +99,6 @@
                                     <input type="button" value="+" class="plus" field="quantity">
                                 </div>
                             </td>
-                            @if(@$feature['coupon'] == 1)
-                            <td>
-                                <span class="disc_indv{{$item->rowid}} disc_indv" rowid='{{$item->rowid}}'> 0.00</span>
-                            </td>
-                            @endif
                             @if(@$feature['tax'] == 1)
                             <td>
                                 <span class="tax_{{$item->rowid}} tax_amt" rowid='{{$item->rowid}}'> {{  number_format($item->options->tax_amt * Session::get('currency_val'), 2, '.', '')}}</span>
@@ -116,7 +108,6 @@
                                 <?php $subTotalAmt = ($item->options->tax_type == 2 ) ? $item->subtotal + $item->options->tax_amt : $item->subtotal; ?>
                                 <span data-pid="{{$item->options->sub_prod}}"  class="get_do subtotal{{$item->rowid}}">{{ number_format($subTotalAmt * Session::get('currency_val'), 2, '.', '')}}</span>
                             </td>
-
                         </tr>
                         @endforeach
                     </tbody>
@@ -137,14 +128,12 @@
                                 <th><div class="black-bg">Total:<span class="currency-sym-in-braces"></span> </div></th>
                                 <td><div class="black-bg"><strong><span class="amount finalAmt">{{ number_format($cart_amt['total']* Session::get('currency_val'), 2, '.', '') }}</span></strong></div></td>
                             </tr>
-
                         </table>
                     </div>
                 </div><!-- .cal-shipping -->
             </div>
             <div class="clearfix"></div>
             <div class="col-md-12 col-sm-12 col-xs-12">
-
                 <div class="cart-actions clearfix">
                     <form action="#">
                         <a class="button bold default" href="{{route('checkout')}}">PROCEED TO CHECKOUT</a>
@@ -169,11 +158,7 @@
         var qty = thisQty.val();
         var maxvalue = parseInt(thisQty.attr('max'));
         var currentVal = parseInt(thisQty.val());
-
         if (!isNaN(currentVal) && (currentVal <= (maxvalue))) {
-            // Increment
-            //$('.plus').css('pointer-events', '');
-            //  alert(parseInt(thisQty.val()));
             thisQty.val(parseInt(currentVal));
             var rowid = thisQty.attr('data-rowid');
             var productId = thisQty.attr('data-productid');
@@ -192,7 +177,6 @@
                     }
                     $(".tax_" + rowid).text((result['tax'] * <?php echo Session::get('currency_val'); ?>).toFixed(2));
                     $('.shop-cart').text(result['cart_count']);
-
                 }
             });
         } else if (currentVal >= maxvalue) {
@@ -209,22 +193,18 @@
                 }
             });
             alert("Specified quantity is not available!");
-
         } else {
             // Otherwise put a 0 there
             thisQty.val(1);
         }
-
     }
     $(document).on("click", '.plus', function () {
-        //alert("sdf");
         var fieldName = $(this).attr("field");
         //   var maxValue = parseInt($('input[name="quantity"]').attr('max'));
         var thisQty = $(this).parent().find('input[name=' + fieldName + ']');
         //  $(this).parent().find('input[name=' + fieldName + ']').val();
         thisQty.val(parseInt(thisQty.val()) + 1);
         calc(thisQty);
-//                $(this).parent().find('input[name=' + fieldName + ']').val(currentVal + 1);
     });
 
     $(document).on("click", '.minus', function () {
@@ -245,25 +225,6 @@
 //                $(this).parent().find('input[name=' + fieldName + ']').val(currentVal + 1);
     });
     $(document).ready(function () {
-//        $(".qty").bind('keyup mouseup', function () {
-//            var thisQty = $(this);
-//            calc(thisQty);
-//        });
-
-//        $(".minus").click(function () {
-//            var thisQty = $('#quantity');
-//             thisQty.val(parseInt(thisQty.val()-1));
-//           // alert(thisQty.val());
-//            calc(thisQty);
-//
-//        });
-//         $(".plus").click(function () {
-//            var thisQty = $('#quantity');
-//              thisQty.val(parseInt(thisQty.val())+1);
-//           // alert(thisQty.val());
-//            calc(thisQty);
-//        });
-
         $(".removeCartItem").click(function () {
             var getRowid = $(this).attr("data-rowid");
             var getRemoveEle = $(this);
@@ -295,128 +256,6 @@
             });
         });
     });
-
-    $("#couponApply").click(function () {
-        var couponCode = $(".userCouponCode").val();
-
-        //alert(couponCode);
-        $.ajax({
-            url: "{{ route('check_coupon') }}",
-            type: 'POST',
-            data: {couponCode: couponCode},
-            cache: false,
-            success: function (msg) {
-                $.each(msg['cart'], function (i, v) {
-                    var sub_total = v.subtotal;
-                    console.log("SUB TOTAL " + sub_total);
-                    $.each(v, function (key, val) {
-                        console.log(typeof val);
-                        if (typeof val === 'object') {
-                            console.log("TAX VAL " + JSON.stringify(val.options));
-                            if (val.tax_amt) {
-                                console.log("TAX PRESENT");
-//                        $(".tax_" + i).text((parseFloat(val.tax_amt) * parseFloat()).toFixed(2));
-                                $(".tax_" + i).text(parseFloat(val.tax_amt * <?php echo Session::get('currency_val'); ?>).toFixed(2));
-                                if (val.tax_type == 2) {
-                                    sub_total = (sub_total + val.tax_amt);
-                                }
-                            }
-                            $(".subtotal" + i).text((sub_total * <?php echo Session::get('currency_val'); ?>));
-                        }
-                    });
-                    $(".subtotal" + i).text((sub_total * <?php echo Session::get('currency_val'); ?>).toFixed(2));
-                });
-                //alert("check_coupon response "+ msg );
-                $(".cmsg").css("display", "block");
-                if (msg['remove'] == 1) {
-                    $(".cmsg").html(msg['cmsg']);
-                    $("#couponUsedAmount").text("0.00");
-                    $("#couponApply").removeAttr("disabled");
-                    $("#userCouponCode").val('');
-                    $("#userCouponCode").removeAttr("disabled");
-                    $(".coupCode").text(" ");
-                    // $(".allSubtotal").text(msg['subtotal'].toFixed(2));
-                    $(".finalAmt").text(parseFloat((msg['orderAmount'])).toFixed(2));
-                    $(".disc_indv").text("0.00");
-                    $("#amountallSubtotal").text(parseFloat((msg['subtotal'])).toFixed(2));
-                } else {
-                    //  alert('test............');
-                    $("#couponApply").attr("disabled", "disabled");
-                    $(".userCouponCode").attr("disabled", "disabled");
-                    var coupon_msg = "<span style='color:green;'>Coupon Applied!</span> <a href='javascript:void(0);' style='border-bottom: 1px dashed;' class='clearCoup'>Remove!</a>";
-                    $(".coupCode").text("[" + $(".userCouponCode").val() + "]");
-                    var newAmount = msg['orderAmount'];
-                    // var newAmount = msg['new_amt'];
-                    var usedCouponAmount = (msg['disc'] * <?php echo Session::get('currency_val'); ?>).toFixed(2);
-                    // console.log(msg['cart']);
-                    $.each(msg['cart'], function (key, value) {
-                        $(".disc_indv" + key).text((parseFloat(value.options.disc * <?php echo Session::get('currency_val'); ?>)).toFixed(2));
-                        // console.log('-------sss------'+key);
-                    });
-                    $(".finalAmt").text(parseFloat((newAmount)).toFixed(2));
-                    $("#couponUsedAmount").text(parseFloat((usedCouponAmount)).toFixed(2));
-                    $(".cmsg").html(coupon_msg);
-                    $("#amountallSubtotal").text(parseFloat((msg['subtotal'])).toFixed(2));
-
-                }
-            }
-        });
-        return false;
-    });
-    //remove coupon
-    $("body").on("click", ".clearCoup", function () {
-        var couponCode = '';
-        $.ajax({
-            url: "{{ route('check_coupon') }}",
-            type: 'POST',
-            data: {couponCode: couponCode},
-            cache: false,
-            success: function (msg) {
-                // alert(msg);
-                console.log('msg' + msg['remove']);
-                if (msg['remove'] == 1) {
-                    $.each(msg['cart'], function (i, v) {
-                        var sub_total = v.subtotal;
-                        $.each(v, function (key, val) {
-                            console.log(typeof val);
-                            if (typeof val === 'object') {
-                                console.log("Remove " + JSON.stringify(val));
-                                if (val.tax_amt) {
-                                    console.log("TAX PRESENT");
-                                    $(".tax_" + i).text(parseFloat((val.tax_amt) * <?php echo Session::get('currency_val'); ?>).toFixed(2));
-                                    if (val.tax_type == 2) {
-                                        sub_total = sub_total + val.tax_amt;
-                                    }
-                                    $(".subtotal" + i).text((parseFloat(sub_total * <?php echo Session::get('currency_val'); ?>)).toFixed(2));
-                                }
-                            }
-                        });
-                        $(".subtotal" + i).text((parseFloat(sub_total * <?php echo Session::get('currency_val'); ?>)).toFixed(2));
-                    });
-                    $("#couponUsedAmount").text("0.00");
-                    $(".cmsg").html("Coupon Removed!");
-                    $(".coupCode").text(" ");
-                    $("#couponApply").removeAttr("disabled");
-                    $(".userCouponCode").removeAttr("disabled");
-                    $(".finalAmt").text((msg['orderAmount']).toFixed(2));
-                    $(".allSubtotal").text((msg['subtotal']).toFixed(2));
-                    $(".disc_indv").text("0.00");
-                    $('.userCouponCode ').val('');
-                }
-            }
-        });
-    });
-    var chkcouponapplied = "";
-<?php if (!empty(Session::get('usedCouponId'))) { ?>
-        var chkcouponapplied = <?php echo Session::get('usedCouponId'); ?>;
-        var copCode = <?php echo "'" . Session::get('usedCouponCode') . "'"; ?>;
-<?php } ?>
-    if (chkcouponapplied) {
-        $("#couponApply").attr("disabled", "disabled");
-        $(".userCouponCode").attr("disabled", "disabled");
-        $(".userCouponCode").val(copCode);
-        $("#couponApply").click();
-    }
     function isNumber(evt) {
         evt = (evt) ? evt : window.event;
         var charCode = (evt.which) ? evt.which : evt.keyCode;
@@ -425,48 +264,5 @@
         }
         return true;
     }
-//    jQuery(document).ready(function () {
-//        // This button will increment the value
-//        $('.plus').click(function (e) {
-//            console.log('Inside');
-//            // Stop acting like a button
-//            e.preventDefault();
-//            // Get the field name
-//            var fieldName = $(this).attr('field');
-//            var maxvalue = parseInt($('input[name="quantity"]').attr('max'));
-//            console.log('Inside' + maxvalue);
-//            console.log('Inside fieldName' + fieldName);
-//            // Get its current value
-//            var currentVal = parseInt($('input[name=' + fieldName + ']').val());
-//            // If is not undefined
-//            if (!isNaN(currentVal) && (currentVal <= maxvalue)) {
-//                // Increment
-//                $('input[name=' + fieldName + ']').val(currentVal + 1);
-//            } else if (currentVal >= maxvalue) {
-//                alert("Specified quantity is not available!");
-//            } else {
-//                // Otherwise put a 0 there
-//                $('input[name=' + fieldName + ']').val(0);
-//            }
-//        });
-//        // This button will decrement the value till 0
-//        $(".minus").click(function (e) {
-//            // Stop acting like a button
-//            e.preventDefault();
-//            // Get the field name
-//            fieldName = $(this).attr('field');
-//            // Get its current value
-//            var currentVal = parseInt($('input[name=' + fieldName + ']').val());
-//            // If it isn't undefined or its greater than 0
-//            if (!isNaN(currentVal) && currentVal > 0) {
-//                // Decrement one
-//                $('input[name=' + fieldName + ']').val(currentVal - 1);
-//            } else {
-//                // Otherwise put a 0 there
-//                $('input[name=' + fieldName + ']').val(0);
-//            }
-//        });
-//    });
 </script>
-
 @stop
