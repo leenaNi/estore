9c5c0dbeed0eed9c5f5c2dbde89950f8bbca0fc1
leenan <leena@infiniteit.biz>
2018-09-04 19:01:01 +0530

    cart img

--- mall/resources/views/Frontend/pages/cart/index.blade.php
+++ mall/resources/views/Frontend/pages/cart/index.blade.php
@@ -12,7 +12,7 @@
 </section>
 <div id="content" class="site-content single-product">
     <div class="container">
-        {{dd($cart);}}
+        {{print_r($cart)}}
         @if(!empty($cart->toArray()))
         <form class="cart-form" action="#">
             <div class="table-responsive">
