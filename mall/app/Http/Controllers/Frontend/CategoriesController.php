26f506bd50e40e52ee529fce2d15ed09ffd0bb4a
leenan <leena@infiniteit.biz>
2018-09-05 19:07:39 +0530

    cart config prod

--- mall/app/Http/Controllers/Frontend/CategoriesController.php
+++ mall/app/Http/Controllers/Frontend/CategoriesController.php
@@ -95,7 +95,7 @@ class CategoriesController extends Controller {
     public function getProductListing() {
 //        dd(Input::all());
         $catzz = json_decode(Input::get('filters'), true);
-//        dd($catzz);
+        dd($catzz);
         $slug = Input::get('slug');
         $maxP = 0;
         $checkVarient = GeneralSetting::where('url_key', 'products-with-variants')->first()->status;
