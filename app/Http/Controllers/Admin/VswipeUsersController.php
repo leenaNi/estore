7bf8d02d47ded591eb687bdea30592b52f172ffb
pradeep <pradeep@infiniteit.biz>
2018-09-06 15:57:32 +0530

    payment settlement

--- app/Http/Controllers/Admin/VswipeUsersController.php
+++ app/Http/Controllers/Admin/VswipeUsersController.php
@@ -15,9 +15,9 @@ use DB;
 class VswipeUsersController extends Controller {
 
     public function index() {
-         $orders=DB::table("has_products")->orderBy("has_products.id","desc")->join("stores","stores.id",'=',"has_products.store_id")->select('has_products.*', 'stores.store_name')->paginate(Config('constants.AdminPaginateNo'));
-       dd($orders);
-         
+//         $orders=DB::table("has_products")->orderBy("has_products.id","desc")->join("stores","stores.id",'=',"has_products.store_id")->select('has_products.*', 'stores.store_name')->paginate(Config('constants.AdminPaginateNo'));
+//       dd($orders);
+//         
          $vusers = VswipeUser::orderBy('id', 'desc');
 
         $search = Input::get('search');
--- resources/views/Admin/Pages/paymentSettlement/index.blade.php
+++ resources/views/Admin/Pages/paymentSettlement/index.blade.php
@@ -17,14 +17,25 @@
     <div class="row">
         <div class="col-xs-12">
             <div class="box">
-
-              
+                <div class="box-header">
+                    <div class="box-header col-md-3">
+                        <form action="" class="formMul" method="post">
+                            <input type="hidden" value="" name="OrderIds" />
+                            <select name="orderAction" id="orderAction" class="form-control pull-right col-md-12">
+                                <option value="">Please Select an Action</option>
+                                <!-- <option value="">Generate Waybill</option> -->
+                                <option value="1">Payment Settlement</option>
+                                <!--<option value="17">Send to Shiprocket</option>-->
+                            </select>
+                        </form> 
+                    </div>
+                </div>
                 <div class="box-body table-responsive">
                     <table class="table table-hover">
                         <tr>
-                 
+                            <th><input type="checkbox" id="checkAll" /></th>
                             <th>Order Id</th>
-                
+
                             <th>Sub order Id</th>
                             <th>Store Name</th>
                             <th>Paid Amount</th>
@@ -35,16 +46,16 @@
                         </tr>
                         @foreach($orders as $order)
                         <tr>
-
+                            <td><input type="checkbox" name="orderId[]" class="checkOrderId" value="{{ $product->id }}" /></td>
                             <td>{{ $order->order_id }}</td>
-                         
+
                             <td>{{ $order->id }}</td>
                             <td>{{ $order->store_name }}</td>
                             <td>{{ $order->pay_amt }}</td>
                             <td>{{ $order->settled_amt }}</td>
                             <td>{{ date('d-M-Y',strtotime($order->settled_date)) }}</td>
-                            
-                       
+
+
                             <td>{{ date('d-M-Y',strtotime($order->created_at)) }}</td>
                             <td>
                             </td>
