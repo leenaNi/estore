22f2e64a5f00a8b5e73ab912cda05d2f042044ba
leenan <leena@infiniteit.biz>
2018-09-11 15:48:09 +0530

    store cart

--- merchants/resources/views/Admin/pages/orders/invoice.blade.php
+++ merchants/resources/views/Admin/pages/orders/invoice.blade.php
@@ -188,7 +188,7 @@ $address = $order->users->addresses->first();
                         print_r($prd['options']['options']);
                         //$warehouseCode = @Product::find($prd['options']['sub_prod'])->warehouse_code;
                         foreach ($prd['options']['options'] as $key => $value) {
-                            echo $key . ": " . DB::table($prd['options']['prefix'] . '_attribute_values')->where('id', $value)->first()->option_name . str_repeat('&nbsp;', 2) . "<br/>";
+                            echo $key . ": " . DB::table('attribute_values')->where('id', $value)->first()->option_name . str_repeat('&nbsp;', 2) . "<br/>";
                         }
                         echo "<br/>SKU: " . $prd['options']['sub_prod'] . str_repeat('&nbsp;', 2) . "WC: " . $warehouseCode;
                     }
@@ -199,7 +199,7 @@ $address = $order->users->addresses->first();
                             if (!empty($value['options'])) {
                                 foreach ($value['options'] as $opt => $optval) {
                                     echo $value['name'] . "<br/>";
-                                    echo $opt . ": " . DB::table($prd['options']['prefix'] . '_attribute_values')->where('id', $optval)->first()->option_name . str_repeat('&nbsp;', 2) . "<br/>SKU: " . $value['sub_prod'] . str_repeat('&nbsp;', 2) . "WC: " . @Product::find($value['sub_prod'])->warehouse_code . "<br/><br/>";
+                                    echo $opt . ": " . DB::table('attribute_values')->where('id', $optval)->first()->option_name . str_repeat('&nbsp;', 2) . "<br/>SKU: " . $value['sub_prod'] . str_repeat('&nbsp;', 2) . "WC: " . @Product::find($value['sub_prod'])->warehouse_code . "<br/><br/>";
                                 }
                             } else {
                                 $simpleProd = app\Models\Product::find($key);
