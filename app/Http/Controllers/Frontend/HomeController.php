4cdd48820e468ae0388467a2226a636fc91965c8
pradeep <pradeep@infiniteit.biz>
2018-09-17 14:12:59 +0530

    resolve error in create stores

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -444,7 +444,7 @@ class HomeController extends Controller {
 
 
                     if (!empty($themeid)) {
-                        $themedata = DB::select("SELECT t.id,c.category,t.theme_category,t.image from themes t left join categories c on t.cat_id=c.id order by c.category where t.cat_id = $catid");
+                        $themedata = DB::select("SELECT t.id,c.category,t.theme_category,t.image from themes t left join categories c on t.cat_id=c.id where t.cat_id = ".$catid." order by c.category" );
                         $decodeVal['theme'] = strtolower(StoreTheme::find($themeid)->theme_category);
                         $decodeVal['themeid'] = $themeid;
                         $decodeVal['themedata'] = $themedata;
