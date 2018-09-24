1f4243aabb15469e76adbaed3265179cc3d40a4a
leenan <leena@infiniteit.biz>
2018-09-24 11:39:40 +0530

    asd

--- app/Http/Controllers/Admin/PaymentSettlementController.php
+++ app/Http/Controllers/Admin/PaymentSettlementController.php
@@ -104,8 +104,8 @@ class PaymentSettlementController extends Controller {
         $orders = DB::table("has_products")->orderBy("has_products.id", "desc")->join("stores", "stores.id", '=', "has_products.store_id")->
                 leftjoin("payment_settlement", "payment_settlement.order_id", '=', "has_products.id")
                 ->join("orders", "orders.id", '=', "has_products.order_id")
-//              
-                ->select(DB::raw('sum(has_products.pay_amt) as totalOrder, (select sum(has_products.pay_amt) from orders where orders.courier = 0) as c3, (select sum(has_products.pay_amt) from orders where orders.courier = 1) as c4, stores.store_name, sum(payment_settlement.settled_amt) as totalPaid, sum(payment_settlement.order_amt) as orderAmt'))
+//              , (select sum(has_products.pay_amt) from orders where orders.courier = 0) as c3, (select sum(has_products.pay_amt) from orders where orders.courier = 1) as c4,
+                ->select(DB::raw('sum(as_products.pay_amt) as totalOrder, stores.store_name, sum(payment_settlement.settled_amt) as totalPaid, sum(payment_settlement.order_amt) as orderAmt'))
                 ->groupBy("has_products.store_id");
         $orders = $orders->paginate(Config('constants.AdminPaginateNo'));
         dd($orders);
