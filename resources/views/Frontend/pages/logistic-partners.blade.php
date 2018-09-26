67a94c6190688d37f9ec97004482162217f3e38b
vikramgangawane <vikram.g@infiniteit.biz>
2018-09-26 12:00:21 +0530

    add tab in logistic partner page

--- resources/views/Frontend/pages/logistic-partners.blade.php
+++ resources/views/Frontend/pages/logistic-partners.blade.php
@@ -13,38 +13,48 @@
 			<div class="content-wrap">
 
 				<div class="container clearfix">
-				<div class="tabs tabs-alt tabs-tb clearfix" id="tab-8">
-
-<ul class="tab-nav clearfix">
-	<li><a href="#tabs-29">eCommerce(BanglaDesh)</a></li>
-	<!-- <li><a href="#tabs-30">Nunc tincidunt</a></li> -->
-</ul>
-
-<div class="tab-container">
-
-	<div class="tab-content clearfix" id="tabs-29">
-	<div class="table-responsive">
-
-<table class="table table-hover table-bordered table-comparison nobottommargin">
-	<thead>
-	<tr>
-		<th>Area</th>
-		<th>Currency(<i class="icon-taka"></i>)</th>
-	</tr>
-	</thead>
-	<tbody>
-	<tr>
-		<td>1K</td>
-		<td>100 <i class="icon-taka"></i></td>
-	</tr>
-	</tbody>
-</table>
-
-</div>
-	</div>
-	<!-- <div class="tab-content clearfix" id="tabs-30">
-		Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.
-	</div> -->
+					<div class="tabs tabs-alt tabs-tb clearfix" id="tab-8">
+						<ul class="tab-nav clearfix">
+							<li><a href="#tabs-bang">eCommerce(Bangladesh)</a></li>
+							<li><a href="#tabs-ind">eCommerce(India)</a></li>
+						</ul>
+						<div class="tab-container">
+							<div class="tab-content clearfix" id="tabs-bang">
+								<div class="table-responsive">
+									<table class="table table-hover table-bordered table-comparison nobottommargin">
+										<thead>
+										<tr>
+											<th>Area</th>
+											<th>Currency(<i class="icon-taka"></i>)</th>
+										</tr>
+										</thead>
+										<tbody>
+										<tr>
+											<td>1K</td>
+											<td>100 <i class="icon-taka"></i></td>
+										</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+							<div class="tab-content clearfix" id="tabs-ind">
+								<div class="table-responsive">
+									<table class="table table-hover table-bordered table-comparison nobottommargin">
+										<thead>
+										<tr>
+											<th>Area</th>
+											<th>Currency(<i class="icon-rupee"></i>)</th>
+										</tr>
+										</thead>
+										<tbody>
+										<tr>
+											<td>1K</td>
+											<td>100 <i class="icon-Rupee"></i></td>
+										</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
 	
 
 </div>
