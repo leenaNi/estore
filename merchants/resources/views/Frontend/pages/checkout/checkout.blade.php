35cd2e3e52fa46cc107e0a5df146e3aaec72cdf6
vikramgangawane <vikram.g@infiniteit.biz>
2018-10-05 17:48:25 +0530

    update new banglades contact address

--- merchants/resources/views/Frontend/pages/checkout/checkout.blade.php
+++ merchants/resources/views/Frontend/pages/checkout/checkout.blade.php
@@ -358,7 +358,7 @@
                                                             <div class="form-group"  >
                                                                 <div class="col-sm-12 col-xs-12"> 
                                                                     <p class="blue_text">Applicable only for first time users.</p>
-                                                                    <p class="referalMsg" style="color:red;font-size:13px;margin-top:15px" ></p>
+                                                                    <p class="" style="color:red;font-size:13px;margin-top:15px" >asdf</p>
                                                                 </div>
                                                                 <label for="email" class="col-md-12">REFERRAL</label>
                                                                 <p class="col-md-8"><input name="require_referal" type="text" class="form-control requireReferal cartinput" placeholder="Enter Referral Code"></p>
