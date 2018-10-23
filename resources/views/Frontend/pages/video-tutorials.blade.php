a924b232a84275b6aa777e2045a63fd85659fe37
vikramgangawane <vikram.g@infiniteit.biz>
2018-10-23 11:57:24 +0530

    create new page for videotutorials

--- resources/views/Frontend/pages/video-tutorials.blade.php
+++ resources/views/Frontend/pages/video-tutorials.blade.php
@@ -6,27 +6,21 @@
     <!-- Page Title
 		============================================= -->
 		<section id="page-title" class="page-title-parallax page-title-center" style="background: url('{{ asset(Config('constants.frontendPublicImgPath').'/static.jpg') }}') 0px; padding: 103px 0;">
-
-	<div class="container clearfix">
+			<div class="container clearfix">
 				<h2 class="white-text bottommargin-xs" data-animate="fadeInUp">Registration Process</h2>
 				<!-- <span class="white-text" data-animate="fadeInUp" data-delay="300">One powerful platform. Two simple plans.</span> -->
 			</div>
-</section>
-<!-- #page-title end -->
-  <section id="content">
-	<div class="content-wrap">
-
-<div class="container clearfix aboutPage">
-<div class="col_full nobottommargin">
-<iframe style="max-width:100%; height:400px;"  src="https://www.youtube.com/embed/P2tEViNXn4Q" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+		</section>
+	<!-- #page-title end -->
+		<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+					<div class="col_full nobottommargin">
+						<iframe style="max-width:100%; height:400px;"  src="https://www.youtube.com/embed/P2tEViNXn4Q" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 					</div>
 					<div class="clearfix"></div>
-</div>
-</div>
-
-
-
-
+				</div>
+			</div>
 		</section>
 		@stop
 
