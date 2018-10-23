346a4ceaf2312c0689f4fd8a17cba1deb7a49696
vikramgangawane <vikram.g@infiniteit.biz>
2018-10-23 11:48:08 +0530

    create new page for videotutorials

--- resources/views/Frontend/pages/video-tutorials.blade.php
+++ resources/views/Frontend/pages/video-tutorials.blade.php
@@ -5,13 +5,12 @@
 
     <!-- Page Title
 		============================================= -->
-		<section id="page-title" class="page-title-parallax page-title-center" style="background: url('{{ asset(Config('constants.frontendPublicImgPath').'/about-bg.jpg') }}') 0px; padding: 40px 0;">
+		<section id="page-title" class="page-title-parallax page-title-center" style="background: url('{{ asset(Config('constants.frontendPublicImgPath').'/static.jpg') }}') 0px; padding: 103px 0;">
 
-<div class="container clearfix">
-<img src="{{ asset(Config('constants.frontendPublicImgPath').'/about-image.png') }}" alt="Offline to Online">
-<h3 class="white-text topmargin-xs bottommargin-xs" data-animate="fadeInUp">Give a digital dimension to your physical store</h3>
-	<!--<span class="white-text" data-animate="fadeInUp" data-delay="300">We are trying to expedite the process by providing superior technology and online assistance.</span> -->
-</div>
+	<div class="container clearfix">
+				<h2 class="white-text bottommargin-xs" data-animate="fadeInUp">Registraion Process</h2>
+				<!-- <span class="white-text" data-animate="fadeInUp" data-delay="300">One powerful platform. Two simple plans.</span> -->
+			</div>
 </section>
 <!-- #page-title end -->
   <section id="content">
