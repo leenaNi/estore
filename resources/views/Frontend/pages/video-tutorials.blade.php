4823496271fd8678b07f807f5248482b55a25af4
vikramgangawane <vikram.g@infiniteit.biz>
2018-10-23 12:37:40 +0530

    create new page for videotutorials

--- resources/views/Frontend/pages/video-tutorials.blade.php
+++ resources/views/Frontend/pages/video-tutorials.blade.php
@@ -15,10 +15,9 @@
 		<section id="content">
 			<div class="content-wrap">
 				<div class="container clearfix">
-					<div class="col_full nobottommargin">
-					<embed src="https://www.youtube.com/embed/P2tEViNXn4Q" wmode="transparent" type="application/x-shockwave-flash" width="100%" height="100%" allowfullscreen="true" title="Adobe Flash Player">
-						
-					</div>
+					
+						<iframe style="max-width:100%; height:400px;"  src="https://www.youtube.com/embed/P2tEViNXn4Q" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+					
 					<div class="clearfix"></div>
 				</div>
 			</div>
