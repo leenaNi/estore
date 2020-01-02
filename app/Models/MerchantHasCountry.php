1a40b5d697a358c9b9da34d9bd5060785abfc9b9
Anita Kapal <anita@infiniteit.biz>
2020-01-02 11:12:11 +0530

    merchanch country changes

--- app/Models/MerchantHasCountry.php
+++ app/Models/MerchantHasCountry.php
@@ -9,6 +9,11 @@ class MerchantHasCountry extends Model
     protected $table = 'merchant_has_countries';
     protected $fillable = ['merchant_id', 'country_id'];
 
-    
+    public function countries()
+    {
+        return $this->belongsTo('App\Models\Country', 'name');
+        // $prod->attributes()->sync($attributes);
+        // $prod->attributes()->detach();
+    }
 
 }
