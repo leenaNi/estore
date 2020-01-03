afd7d9d55065da44efbbbce84853eedd5f96328b
leenan <leena@infiniteit.biz>
2020-01-03 17:02:44 +0530

    contacts

--- merchants/app/Models/Contact.php
+++ merchants/app/Models/Contact.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
+    protected $table = 'contacts';
     protected $fillable = ['phone_no', 'email', 'address', 'status'];
+    public function newQuery($excludeDeleted = true)
+    {
+        return parent::newQuery($excludeDeleted = true)
+            ->where('store_id', Helper::getSettings()['store_id']);
+    }
 }
