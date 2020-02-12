be7ce3e36b7c938f7274849a12cf33c17c0fe810
leenan <leena@infiniteit.biz>
2020-02-12 11:54:21 +0530

    otp

--- app/Http/Controllers/Admin/ApiMerchantController.php
+++ app/Http/Controllers/Admin/ApiMerchantController.php
@@ -25,7 +25,7 @@ class ApiMerchantController extends Controller
         $phone = Input::get("phone");
         if (Input::get("phone") && !empty(Input::get("phone"))) {
             $otp = rand(1000, 9999);
-            $userdata = DB::table('users')->where('telephone', $phone)->where('user_type', 1)->first();
+            $userdata = User::where('telephone', $phone)->where('user_type', 1)->first();
             if (!empty($userdata)) {
                 $userdata->otp = $otp;
                 $userdata->save();
