359d2bd219891306d207d62862c2394d983d5a3a
leenan <leena@infiniteit.biz>
2020-02-17 18:57:58 +0530

    api method change

--- routes/api.php
+++ routes/api.php
@@ -253,8 +253,8 @@ Route::group(['namespace' => 'Admin'], function () {
         });
         
         Route::group(['prefix' => 'cart'], function () {
-            Route::any('', ["as" => "admin.cart.view", "uses" => "ApiCartController@index"]);
-            Route::any('/add', ["as" => "admin.cart.add", "uses" => "ApiCartController@add"]);
+            Route::post('', ["as" => "admin.cart.view", "uses" => "ApiCartController@index"]);
+            Route::post('/add', ["as" => "admin.cart.add", "uses" => "ApiCartController@add"]);
         });
     });
 });
