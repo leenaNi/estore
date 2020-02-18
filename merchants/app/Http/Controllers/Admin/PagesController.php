8cb521838927a062c57606ce600229ec74db261b
leenan <leena@infiniteit.biz>
2020-02-18 11:50:53 +0530

    product price

--- merchants/app/Http/Controllers/Admin/PagesController.php
+++ merchants/app/Http/Controllers/Admin/PagesController.php
@@ -159,8 +159,9 @@ class PagesController extends Controller
         foreach ($topProducts as $key => $product) {
             if ($product->prod_id == $product->sub_prod_id || $prd->sub_prod_id == '') {
                 $parentprod = Product::find($product->prod_id);
-                if($parentprod != null){
+                if ($parentprod != null) {
                     $product->product->price += $parentprod->selling_price;
+                    $product->product->actualPrice = $product->product->price + $parentprod->selling_price;
                 }
             }
             $products[$key]["product_name"] = $product->product;
