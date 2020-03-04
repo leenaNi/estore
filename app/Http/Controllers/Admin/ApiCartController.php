de49b84d329fe8103f633c0eb38857638a30b0a2
leenaNic <leena@infiniteit.biz>
2020-03-04 15:28:24 +0530

    offer price

--- app/Http/Controllers/Admin/ApiCartController.php
+++ app/Http/Controllers/Admin/ApiCartController.php
@@ -410,7 +410,7 @@ class ApiCartController extends Controller
                                     else if($product->prod_type==3 || $product->parent_prod_id!=0){  
                                         $msg = $this->configProduct($product->parent_prod_id, $offerProd->qty,$product->id);
                                     }
-                                    //dd($msg);
+                                    dd($msg);
                                     if ($msg == 1) {
                                         $data['data']['cart'] = null;
                                         $data['status'] = "0";
