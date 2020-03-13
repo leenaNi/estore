f4f23548f422678bebfca4a26378dca0ca4f2eaa
leenan <leena@infiniteit.biz>
2020-03-13 10:26:01 +0530

    postlogin

--- app/Http/Middleware/authJWT.php
+++ app/Http/Middleware/authJWT.php
@@ -42,7 +42,7 @@ class authJWT
             header("Access-Control-Allow-Origin: *");
             header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
             header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Authorization, Origin');
-            // Helper::postLogin($user);
+            Helper::postLogin($user);
 
         } catch (Exception $e) {
 
