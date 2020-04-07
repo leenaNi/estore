1c4bdf44f5be7862c6bee3463953eb2185b06b40
Anita Kapal <anita@infiniteit.biz>
2020-04-07 11:50:02 +0530

    resolved error in dashboard

--- merchants/app/Http/Controllers/Admin/PagesController.php
+++ merchants/app/Http/Controllers/Admin/PagesController.php
@@ -360,7 +360,7 @@ class PagesController extends Controller
             ->elementLabel("Product Sales")
             ->dimensions(460, 500)
             ->labels($Date_range)
-            ->values($qty)
+            ->values([10,20,30])
             ->responsive(false);
             
 
