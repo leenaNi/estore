80672ba695e64d89ab304e556927868f54110952
leenan <leena@infiniteit.biz>
2020-04-23 13:39:33 +0530

    supplier menu

--- merchants/resources/views/Admin/includes/supplierSidebar.blade.php
+++ merchants/resources/views/Admin/includes/supplierSidebar.blade.php
@@ -13,46 +13,13 @@
                 </a>
                 <ul class="treeview-menu">
                     <li class="{{ preg_match("/admin.products/",Route::currentRouteName()) ? 'active' : '' }}"><a href="{{ route('admin.products.view') }}"><i class="fa fa-angle-right"></i>All Products</a></li>
-                
-
-                    @if($settingStatus['products-with-variants'] == 1)
-
-                    <li class="{{ preg_match("/admin.attribute.set/",Route::currentRouteName()) ? 'active' : '' }}"><a href="{{ route('admin.attribute.set.view') }}"><i class="fa fa-angle-right"></i>Variant Sets</a></li>
-                    <li class="{{ preg_match("/admin.attributes/",Route::currentRouteName()) ? 'active' : '' }}"><a href="{{ route('admin.attributes.view') }}"><i class="fa fa-angle-right"></i>Attributes</a></li>
-                    @endif
-
-                    @if($feature['stock'] == 1)
-                    <li class="{{ preg_match("/admin.stock/",Route::currentRouteName()) ? 'active' : '' }}"">
-                        <a href="#"><i class="fa fa-angle-right"></i>Inventory</a>
-                        <ul class="treeview-menu">
-                            <li class="treeview {{ preg_match("/admin.stock.view/",Route::currentRouteName()) ? 'active' : '' }}">
-                                <a href="{{ route('admin.stock.view') }}"><i class="fa fa-angle-right"></i>In Stock</a>
-                            </li>
-                            <li class="treeview {{ preg_match("/admin.stock.runningShort/",Route::currentRouteName()) ? 'active' : '' }}">
-                                <a href="{{ route('admin.stock.runningShort') }}"><i class="fa fa-angle-right"></i>Running Short</a>
-                            </li>
-                            <li class="treeview {{ preg_match("/admin.stock.outOfStock/",Route::currentRouteName()) ? 'active' : '' }}">
-                                <a href="{{ route('admin.stock.outOfStock') }}"><i class="fa fa-angle-right"></i>Out of Stock</a>
-                            </li>
-
-
-                        </ul>
-                    </li>
-                    @endif
-                    @if($feature['tax']==1)
-                    <li class="{{ preg_match("/admin.tax/",Route::currentRouteName()) ? 'active' : '' }}"><a  href="{{ route('admin.tax.view') }}"><i class="fa fa-angle-right"></i>Taxes</a></li>
-                    @endif
-
-                    @if($feature['row-material'] == 1)
-                    <li class="{{ preg_match("/admin.raw-material/",Route::currentRouteName()) ? 'active' : '' }}"><a  href="{{ route('admin.raw-material.view') }}"><i class="fa fa-angle-right"></i>Raw Material</a></li>
-                    @endif
 
                 </ul>
             </li>
-           
-            
+
+
         </ul>
-    
+
     </section>
 
 </aside>
